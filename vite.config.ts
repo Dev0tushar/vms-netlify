74943015447a7bfda0ef05f00ac5d3923b17e6e1
Dev0tushar <tsharma13791@gmail.com>
2024-08-21 12:18:12 +0530

    error solve code

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       external: ['../axios'],
+//     },
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,8 +21,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    // Remove external option
     rollupOptions: {
-      external: ['/axios'],
+      // No need to externalize axios
     },
   }
 })
