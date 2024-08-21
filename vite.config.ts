db616a1146dda12ee5f4703a154c9ef69969dc02
Dev0tushar <tsharma13791@gmail.com>
2024-08-21 12:05:20 +0530

    update initail code

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       external: ['axios'],
+//     },
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      external: ['axios'],
+      external: [], // Keep this empty if you don't want to externalize axios
     },
+  },
+  optimizeDeps: {
+    include: ['axios'],
   }
 })
